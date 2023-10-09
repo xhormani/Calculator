9091a3664275f08f432945ffa581f53ea1d4cfb0
xhormani <107677882+xhormani@users.noreply.github.com>
2023-10-09 20:09:26 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Choose an operation (+, -, *, /): ")
+        if operation not in ('+', '-', '*', '/'):
+            print("Invalid operation! Please choose +, -, *, or /.")
+        else:
+            if operation == '+':
+                result = add(num1, num2)
+            elif operation == '-':
+                result = subtract(num1, num2)
+            elif operation == '*':
+                result = multiply(num1, num2)
+            elif operation == '/':
+                result = divide(num1, num2)
+            print("Result: ", result)
+            break
+    except ValueError:
+        print("Invalid input! Please enter valid numbers.")
+
